c0ea9009d5ac948577f3d0a5fb0540b861a64371
Nickolas <nickolaslopes.developer@gmail.com>
2025-04-27 10:38:24 -0300

    fixed exercise

--- 1080 - Maior e Posição.py
+++ 1080 - Maior e Posição.py
@@ -1,11 +1,12 @@
-n = int(input())
 cont = 1
 maior = 0
 posicao = 1
-while cont < 10:
+while cont <= 100:
     n = int(input())
     if n > maior:
         maior = n
+        posicao = cont
     cont += 1
 print(maior)
+print(posicao)
 
